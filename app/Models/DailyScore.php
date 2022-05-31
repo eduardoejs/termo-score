cf37c1f3addaa7ff3444b916ba8e129e7b85dd02
Eduardo Silva <eduardo.ejs@hotmail.com>
2022-05-31 10:26:29 -0300

    T-7: refactoring game id cleaning to a mutator instead of a event listener

--- app/Models/DailyScore.php
+++ app/Models/DailyScore.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,10 +10,30 @@ class DailyScore extends Model
 {
     use HasFactory;
 
-    protected static function booted()
+    // booted
+    // protected static function booted()
+    // {
+    //     // quando estiver criando o registro e antes de salvar no DB, faço a transformação do dado, através do listener creating
+    //     static::creating(function($model){
+    //         $model->game_id = str($model->game_id)->replace('#','');
+    //     });
+    // }
+
+    // mutators: forma antiga
+    // public function setGameIdAttribute($value)
+    // {
+    //     $this->attributes['game_id'] = str($value)->replace('#','');
+    // }
+
+    // mutators: forma atual
+    // return new Attribute(
+    //     get: get: fn () => 'edu',
+    //     set: fn ($value) => str($value)->replace('#', '')    
+    // );
+    protected function gameId(): Attribute
     {
-        static::creating(function($model){
-            $model->game_id = str($model->game_id)->replace('#', '');
-        });
+        return new Attribute(            
+            set: fn ($value) => str($value)->replace('#', '')->toString()
+        );
     }
 }
