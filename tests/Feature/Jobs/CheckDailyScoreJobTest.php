b3801a4809fe14bd6de54faa06c20aa8f49e9e19
Eduardo Silva <eduardo.ejs@hotmail.com>
2022-06-10 11:15:08 -0300

    T-9: sending notification when we check the score of the user

--- tests/Feature/Jobs/CheckDailyScoreJobTest.php
+++ tests/Feature/Jobs/CheckDailyScoreJobTest.php
@@ -3,6 +3,8 @@
 use App\Jobs\CheckDailyScoreJob;
 use App\Models\DailyScore;
 use App\Models\WordOfDay;
+use App\Notifications\DailyScoreNotification;
+use Illuminate\Support\Facades\Notification;
 
 it('should calculate the correct points', function ($gameScore, $expectedPoints) {
     
@@ -17,7 +19,6 @@ it('should calculate the correct points', function ($gameScore, $expectedPoints)
     expect($dailyScore->refresh())
         ->points->toBe($expectedPoints)
         ->status->toBe(DailyScore::STATUS_FINISHED);
-
 })->with([
     '10 points'  => ['1/6', 10],
     '5 points'   => ['2/6', 5],
@@ -26,19 +27,19 @@ it('should calculate the correct points', function ($gameScore, $expectedPoints)
     '1 points'   => ['5/6', 1],
     '0 points'   => ['6/6', 0],
     'not enough' => ['X/6', -1],
-// ])->requiresMysql();
+    // ])->requiresMysql();
 ]);
 
 it('should not calculate the points if the word is not the same', function () {
-     // Arrange
-     $dailyScore = DailyScore::factory()->create(['game_id' => 1, 'score' => '1/6', 'word' => 'phone']);
-     $word       = WordOfDay::factory()->create(['word' => 'score', 'game_id' => 1]);
+    // Arrange
+    $dailyScore = DailyScore::factory()->create(['game_id' => 1, 'score' => '1/6', 'word' => 'phone']);
+    $word       = WordOfDay::factory()->create(['word' => 'score', 'game_id' => 1]);
  
-     // Act
+    // Act
      CheckDailyScoreJob::dispatchSync($word, $dailyScore); // force the job run syncronous
  
      // Assert
-     expect($dailyScore->refresh())
+    expect($dailyScore->refresh())
          ->points->toBe(0)
          ->status->toBe(DailyScore::STATUS_WRONG_WORD);
 });
@@ -57,3 +58,22 @@ test('the game id from the wordOfDay should be the same as the dailyScore entry'
         ->status->toBe(DailyScore::STATUS_PENDING);
 });
 
+it('should send a notification to the user whenthe CheckDailyScore is finished', function () {
+    Notification::fake();
+
+    // Arrange
+    $dailyScore = DailyScore::factory()->create(['game_id' => 1, 'score' => '1/6', 'word' => 'score']);
+    $word       = WordOfDay::factory()->create(['word' => 'score', 'game_id' => 1]);
+
+    // Act
+    CheckDailyScoreJob::dispatchSync($word, $dailyScore); // force the job run syncronous
+
+    // Assert
+    Notification::assertSentTo(
+        $dailyScore->user,
+        DailyScoreNotification::class,
+        function (DailyScoreNotification $notification) use ($dailyScore) {
+            return $notification->dailyScore->is($dailyScore);
+        }
+    );
+});
